939fb3cfdc824684320e71352b34ff89b79a0e32
kyleciuk <kyle.ciuk@dteenergy.com>
2026-06-03 10:14:15 -0400

    Update batchNumber to 2 in index.js

--- dist/index.js
+++ dist/index.js
@@ -9438,7 +9438,7 @@ module.exports = class OrganizationUserActivity {
 repositories.sort((a, b) => a.full_name.localeCompare(b.full_name));
 
 const batchSize = 500;
-const batchNumber = 1;
+const batchNumber = 2;
 
 const startIndex = (batchNumber - 1) * batchSize;
 const endIndex = startIndex + batchSize;
