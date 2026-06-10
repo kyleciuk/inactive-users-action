d97ad12ed1ea537c8d48631ceaaaa8ee4b2efa87
kyleciuk <kyle.ciuk@dteenergy.com>
2026-06-10 09:53:35 -0400

    Refactor user details retrieval to use githubClient

--- dist/index.js
+++ dist/index.js
@@ -9531,7 +9531,7 @@ async function process(repo, values, activityType, githubClient) {
       if (userCache[login]) {
         userDetails = userCache[login];
       } else {
-        userDetails = await self.organizationClient.octokit.users.getByUsername({
+        userDetails = await githubClient.users.getByUsername({
           username: login
         });
         userCache[login] = userDetails;
